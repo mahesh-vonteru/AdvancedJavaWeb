4b5c6ee2f92754570e60b07d2d1e9a84d5d4149c
Vish Pepala <vish@claz.org>
2022-07-04 10:00:08 +0530

    Servlets + JSP

--- src/main/java/com/learning/servlets/HelloServlet.java
+++ src/main/java/com/learning/servlets/HelloServlet.java
@@ -1,19 +1,18 @@
 package com.learning.servlets;
 
-import com.learning.jdbc.ConnectionFactory;
-import com.learning.model.ConnectionFactory2;
 import com.learning.model.DBService;
 import com.learning.model.Student;
 
-import java.io.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
